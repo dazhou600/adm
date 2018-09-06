20a1d257daed9f91e3a4fbaff0dc356b83b00cea
dazhou600 <dazhou600@163.com>
2018-09-06 21:05:19 +0800

    home.js level==>json

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,9 +1,10 @@
 //主页导航
 function getNavbarDOM(navbars){
 	var navs = JSON.parse(navbars);
-	//console.log(navs);
+	//console.log(navbars);
 	var countnavs =navs.length;
-	var outstr='';
+	var outstr='[';
+	//var c = a.substring(0,a.lastIndexOf('ba')); //这样就获取到了前面的字符串。
 	for (var i = 0;i< countnavs; i++) {
 		if(i<(countnavs-1)){
 			if(navs[i].level<navs[i+1].level){
@@ -13,43 +14,52 @@ function getNavbarDOM(navbars){
 				//后面没有子节点返回上面
 				outstr = equals(navs[i],outstr,1);
 				outstr = addlabel(navs[i].level-navs[i+1].level,outstr);
-				outstr += '<li role="separator" class="divider"></li>';
 			}else{
 				outstr = equals(navs[i],outstr,2);
 			}
 		}else{
 			outstr = equals(navs[i],outstr,1);
-			outstr = addlabel(navs[i].level-1,outstr);
+			outstr = addlabel(navs[i].level-2,outstr);
+			//if(outstr.lastIndexOf(",")==outstr.length){
+				outstr= outstr.substring(0,outstr.lastIndexOf(','))
+			//}
+			
 		}
 	}
-	$('#navbars').html(outstr);
+	outstr += "]"
+	//var navtree = JSON.parse(outstr);
+	console.log(outstr);
+	//$('#navbars').html(outstr);
 }
 //后面有子节点
 function les(navbar,outstr){
-	console.log("nav***"+outstr);
-	navbar.level>2?outstr +="<li class='dropdown-submenu'>"
-    +"<a href='#'>"+navbar.rname+"<span class='caret'></span></a>"
-    +"<ul class='dropdown-menu'>"
-	:outstr +="<li class='dropdown'>"
-    +"<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>"+navbar.rname+" <span class='caret'></span></a>"
-     +"<ul class='dropdown-menu'>";
+	var tempstr = delRgtBrace(JSON.stringify(navbar));
+	outstr += tempstr+",\""+navbar.rname+"\": [";
 	return outstr;
 }
 /*
  * 同一层加入样式
  * isLast 是当前层级的最后一个?
+ * http://www.bejson.com/
  */
 function equals(navbar,outstr,isLast){
-	outstr +="<li><a href='#'>"+navbar.rname+"<span class='sr-only'>(current)</span></a></li>";
-	if(isLast!==1){
-		outstr += '<li role="separator" class="divider"></li>';
+	outstr += JSON.stringify(navbar);
+	if(isLast==2){
+		outstr +=",";
 	}
 	return outstr;
 }
+
+//去掉右边大括号 }
+function delRgtBrace(str){
+	return str.substring(0,str.lastIndexOf("}"));
+}
+
 //加入结束标签
 function addlabel(level,outstr){
 	for(var j=0;j<level;j++){
-		outstr +="</ul></li>"
+		outstr +="]}"
 	}
+	outstr +=",";
 	return outstr;
 }
---
+++ src/main/resources/static/js/home1.js
@@ -0,0 +1,57 @@
+//主页导航
+function getNavbarDOM(navbars){
+	var navs = JSON.parse(navbars);
+	console.log(navbars);
+	console.log("-------------");
+	console.log(navs);
+	var countnavs =navs.length;
+	var outstr='';
+	for (var i = 0;i< countnavs; i++) {
+		if(i<(countnavs-1)){
+			if(navs[i].level<navs[i+1].level){
+				//如果后面跟着子节点
+				outstr = les(navs[i],outstr);
+			}else if(navs[i].level>navs[i+1].level){
+				//后面没有子节点返回上面
+				outstr = equals(navs[i],outstr,1);
+				outstr = addlabel(navs[i].level-navs[i+1].level,outstr);
+				outstr += '<li role="separator" class="divider"></li>';
+			}else{
+				outstr = equals(navs[i],outstr,2);
+			}
+		}else{
+			outstr = equals(navs[i],outstr,1);
+			outstr = addlabel(navs[i].level-1,outstr);
+		}
+	}
+	$('#navbars').html(outstr);
+}
+//后面有子节点
+function les(navbar,outstr){
+	console.log("nav***"+outstr);
+	navbar.level>2?outstr +="<li class='dropdown-submenu'>"
+    +"<a href='#'>"+navbar.rname+"<span class='caret'></span></a>"
+    +"<ul class='dropdown-menu'>"
+	:outstr +="<li class='dropdown'>"
+    +"<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>"+navbar.rname+" <span class='caret'></span></a>"
+     +"<ul class='dropdown-menu'>";
+	return outstr;
+}
+/*
+ * 同一层加入样式
+ * isLast 是当前层级的最后一个?不加分割样式:否则加分割样式
+ */
+function equals(navbar,outstr,isLast){
+	outstr +="<li><a href='#'>"+navbar.rname+"<span class='sr-only'>(current)</span></a></li>";
+	if(isLast!==1){
+		outstr += '<li role="separator" class="divider"></li>';
+	}
+	return outstr;
+}
+//加入结束标签
+function addlabel(level,outstr){
+	for(var j=0;j<level;j++){
+		outstr +="</ul></li>"
+	}
+	return outstr;
+}
